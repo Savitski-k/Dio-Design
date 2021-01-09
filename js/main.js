1633425331f9c1699dd94b40ed6475fdf32925af
Savitski-k <Savitski.k.v@gmail.com>
2021-01-09 14:41:52 +0300

    js modal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-new WOW().init();
-var mySwiper = new Swiper('.swiper-container', {
-    pagination: {
-        el: '.review-pagination',
-        bulletClass: 'review-bullet',
-        bulletActiveClass: 'review-bullet-active',
-        clickable: true
-    },
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    // modal
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn = document.querySelector('.modal__close');
+    const switchModal = () => {
+        modal.classList.toggle('modal--visible');
+    }
+    modalBtn.forEach(element => {
+        element.addEventListener('click', switchModal);
+    });
+    closeBtn.addEventListener('click', switchModal);
+
+    // slider
+    var mySwiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.review-pagination',
+            bulletClass: 'review-bullet',
+            bulletActiveClass: 'review-bullet-active',
+            clickable: true
+        },
+    });
+
+    // wowJS
+    new WOW().init();
+
 });
