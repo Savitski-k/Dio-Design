4627147f2afae1f9dcbc880d5e862103f18efbc5
Savitski-k <Savitski.k.v@gmail.com>
2021-01-09 11:20:29 +0300

    create new project - site Dio_design

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.review-pagination',
+        bulletClass: 'review-bullet',
+        bulletActiveClass: 'review-bullet-active',
+        clickable: true
+    },
+});
